7bce57fbddaaf6a9a86b9aaeb7effd4b54535ef1
Nadia <nadia.mohamed89@gmail.com>
2021-08-12 19:59:35 +0200

    Add admin boolean to User and create Client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_165633) do
+ActiveRecord::Schema.define(version: 2021_08_12_174651) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "company"
+    t.string "address"
+    t.string "website"
+    t.string "number"
+    t.string "email"
+    t.string "status"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +34,10 @@ ActiveRecord::Schema.define(version: 2021_08_12_165633) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
 end
