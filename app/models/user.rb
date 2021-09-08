cda363eac1b64382fc8734f0d7186a76259e4e3a
Nadia <nadia.mohamed89@gmail.com>
2021-09-08 22:34:16 +0200

    Add Role and Permission new Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_secure_password
+
+  has_many :assignments
+  has_many :roles, through: :assignments
+
+  def role?(role)
+    roles.any? { |r| r.name.underscore.to_sym == role }
+  end
+  
 end
